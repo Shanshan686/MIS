c980abcf76f92af10904da16ee9b5cde7a86066c
Shanshan686 <Shanshan686@users.noreply.github.com>
2019-03-16 15:03:24 -0400

    Update

---
+++ chapter2.py
@@ -0,0 +1,40 @@
+print(3.14e-2)
+
+# length_of_number = len(str(2**1000000))
+
+# print(length_of_number)
+
+print(len('Maddison'))
+import math
+
+print(math.pi)
+print(math.sqrt(25))
+
+import random
+# print(int(random.random()*100))
+# print(random.randint(0,100))
+# print(random.choice([1,4,5,2]))
+#
+# lower = int(input('Please enter a number as the lower bound:'))
+# upper = int(input('Please enter a number as the upper bound:'))
+# print(random.randint(lower,upper))
+
+## strings
+# I'm "Ok".
+print("I'm \"OK\".")
+print('I\'m learning\n\nPython.')
+
+## Boolean
+print(3>2)
+print(3>5)
+
+print(3>2 and 3>5)
+print(3>2 or 3>5)
+
+age = 1
+if age >= 21:
+    print('yes, you can.')
+else:
+    print('sorry, you can\'t.')
+
+# Exercise
